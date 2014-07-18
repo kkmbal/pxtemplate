701f312e216bc3ff385d7d1994961b41b8baf5e2
kkmbal@gmail.com <kkmbal@gmail.com@2eb7d166-5a1c-cc06-f242-5b2cad13b6c5>
2014-07-18 09:02:06 +0000

    20140718

--- portalxpert.template/src/main/java/portalxpert/adm/banner/web/AdmBannerController.java
+++ portalxpert.template/src/main/java/portalxpert/adm/banner/web/AdmBannerController.java
@@ -4,10 +4,12 @@ package portalxpert.adm.banner.web;
 import java.util.List;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
 import javax.servlet.http.HttpSession;
 
 import net.sf.json.JSONArray;
-import net.sf.json.JSONObject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,11 +20,16 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import portalxpert.adm.banner.sc.AdmBannerService;
+import portalxpert.adm.banner.vo.AdmBannerApndFileVO;
 import portalxpert.adm.banner.vo.AdmBannerVO;
+import portalxpert.adm.gen.sc.AdmGenCodeManageService;
+import portalxpert.adm.gen.vo.AdmGenCodeManageVO;
 import portalxpert.common.config.PortalxpertConfigUtils;
-import portalxpert.common.utils.CommUtil;
+import portalxpert.common.utils.FileUploadUtil;
+import portalxpert.common.utils.JSONUtils;
 import portalxpert.common.vo.JSONResult;
 import egovframework.rte.fdl.property.EgovPropertyService;
 import egovframework.rte.ptl.mvc.tags.ui.pagination.PaginationInfo;
@@ -34,9 +41,11 @@ import egovframework.rte.ptl.mvc.tags.ui.pagination.PaginationInfo;
 public class AdmBannerController 
 {
 	
-	/** RmgMenuService */
 	@Resource(name = "admBannerService")
 	private AdmBannerService admBannerService;
+	
+	@Resource(name = "admGenCodeManageService")
+	private AdmGenCodeManageService admGenCodeManageService;
  
 	/** EgovPropertyService */	
 	@Resource(name = "propertiesService")
@@ -73,10 +82,6 @@ public class AdmBannerController
 		admBannerVO.setLastIndex(paginationInfo.getLastRecordIndex());
 		admBannerVO.setRecordCountPerPage(paginationInfo.getRecordCountPerPage());
 	
-//		String useYn = "Y";
-//		if(admBannerVO.getUseYn()!=null && !admBannerVO.getUseYn().equals("") && !admBannerVO.getUseYn().equals("Y")) useYn = admBannerVO.getUseYn();
-//		admBannerVO.setUseYn(useYn);
-		
 		int bannerListCnt = admBannerService.getAdmBannerListToCnt(admBannerVO);
 		paginationInfo.setTotalRecordCount(bannerListCnt);
 		modelMap.put("paginationInfo", paginationInfo);
@@ -86,176 +91,10 @@ public class AdmBannerController
 		modelMap.put("bannerList", bannerList);
 		modelMap.put("admBannerVO", admBannerVO);
 		
-		return ".adm/adm/banner/admBannerList";
+		return ".self/adm/banner/admBannerList";
  	
 	}
-
-	/**
-	 * 홍보배너 등록
-	 * @param AdmSysBannerVO
-	 * @return adm/banner/admBannerInsert
-	 * @exception Exception
-	 */
-	@RequestMapping(value="getAdmBannerRegister")
-	public String getAdmBannerRegister(
-								@ModelAttribute("admBannerVO") AdmBannerVO admBannerVO,
-								ModelMap modelMap)
-								throws Exception {
 	
-		admBannerVO.setPageType("I");
-		
-		//VO 날짜 셋팅
-		if(admBannerVO.getFromDts()=="" || admBannerVO.getFromDts() == null){
-			admBannerVO.setFromDts(CommUtil.getDateString());
-		}
-		if(admBannerVO.getToDts()=="" || admBannerVO.getToDts() == null){
-			admBannerVO.setToDts(CommUtil.getDateString());
-		}
-		modelMap.put("admBanner", admBannerVO);
-		
-		
-		return ".adm/adm/banner/admBannerInsert";
-	}
- 
-	/**
-	 * 파일 서버 업로드
-	 * @param modelMap
-	 * @param session
-	 * @return
-	 * @throws Exception
-	 */
-	@RequestMapping(value = "innoUpload")
-	public String innoUpload(ModelMap modelMap,
-			HttpSession session
-			) throws Exception {
-
-		return ".self/adm/banner/innoUpload";
-	}
-	
-    /**
-     * 배너 첨부이미지 정보 저장
-     * @param FileUpload
-     * @param request
-     * @param response
-     * @param session
-     * @param modelMap
-     * @return
-     * @throws Exception
-     */
-    @RequestMapping(value="/bannerInnoApUpload")
-	public ModelMap bannerInnoApUpload(
-			@RequestParam(value="FileUpload" ,required = true) String FileUpload,
-			@RequestParam(value="bnrId" ,required = true) String bnrId,
-			ModelMap modelMap ,HttpSession session
-			)
-			throws Exception {
-//		String[] file_name = null;
-//	    String[] file_Name_re = null;
-//	    String[] file_size = null;
-//	    String[] web_path = null;
-//	    String IMG_PATH = "/infoBanner/";
-	    
-	 	JSONResult jsonResult = new JSONResult();
-		try {
-			/*
-			logger.debug("FileUpload : " + FileUpload);
-		    String msg = "";
-		    FileUpload = FileUpload.replaceAll("\\},\\{", "\\^");
-		    String[] st= FileUpload.split("\\^");
-//		    System.out.println("]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]st["+st.length+"]::");
-		    file_name = new String[st.length];
-		    file_size = new String[st.length];
-		    file_Name_re = new String[st.length];
-		    web_path = new String[st.length];
-		    
-		    for(int i=0;i<st.length;i++) {
-		    	msg = st[i];
-		    	StringTokenizer st1 = new StringTokenizer(msg,",");
-		    	
-//		    	APPEND_SIZE	saveFileSize
-//		    	APPEND_NM_ORG	original
-//		    	APPEND_NM_RE	saveFileName
-//		    	APPEND_PATH	webDir
-		    	
-		    	while(st1.hasMoreTokens()){
-		    		String elementStr = st1.nextToken();
-		    		String[] elements = elementStr.split(":",2);
-		    		
-		    		String data = elements[1];
-		    		data = data.replaceAll("\"", "");
-		    		data = data.replaceAll("\\}", "");
-		    		data = data.replaceAll("\\]", "");
-		    		
-		    		if(elements[0].indexOf("saveFileSize")>0) file_size[i] = data;
-		    		if(elements[0].indexOf("original")>0) file_name[i] = data;
-		    		if(elements[0].indexOf("saveFileName")>0) file_Name_re[i] = data;
-		    		if(elements[0].indexOf("webDir")>0) web_path[i] = data;
-		    	}
-		    }
-		    
-		    for(int i=0;i<file_name.length;i++){
-//		    	System.out.println("file_size["+i+"]::"+file_size[i]);
-//		    	System.out.println("file_name["+i+"]::"+file_name[i]);
-//		    	System.out.println("file_Name_re["+i+"]::"+file_Name_re[i]);
-//		    	System.out.println("web_path["+i+"]::"+web_path[i]);
-		    	
-		    	AdmBannerVO bannerFileVO = new AdmBannerVO();
-		    	bannerFileVO.setBnrId(bnrId);
-		    	bannerFileVO.setImgSize(Integer.parseInt(file_size[i]));
-		    	bannerFileVO.setImgNameOrg(file_name[i]);
-		    	bannerFileVO.setImgNameReal(file_Name_re[i]);
-		    	bannerFileVO.setImgPath(IMG_PATH);
-		    	
-		    	admBannerService.bannerInnoApUpload(bannerFileVO);
-		    }
-		    */
-			
-			AdmBannerVO delBannerFileVO = new AdmBannerVO();
-			delBannerFileVO.setBnrId(bnrId);
-			admBannerService.deleteAdmBannerAppendImg(delBannerFileVO, session);
-			
-			
-			JSONObject bbsNotiObject = JSONObject.fromObject(FileUpload);
-			JSONArray jsonArr = (JSONArray)bbsNotiObject.get("jsonAppendFileList");
-			for (int i=0; i < jsonArr.size(); i++)
-			{
-				JSONObject obj = (JSONObject)jsonArr.get(i);
-				AdmBannerVO bannerFileVO = new AdmBannerVO();
-				bannerFileVO.setBnrId(bnrId);
-				bannerFileVO.setImgSize(obj.getInt("apndFileSz"));
-				bannerFileVO.setImgNameOrg((String)obj.get("apndFileOrgn"));
-				bannerFileVO.setImgNameReal((String)obj.get("apndFileName"));
-				bannerFileVO.setImgPath((String)obj.get("apndFilePath"));
-		    	
-		    	admBannerService.bannerInnoApUpload(bannerFileVO);
-				
-			}			
-			
-		} catch (Exception e) {
-			jsonResult.setSuccess(false);
- 			jsonResult.setMessage(messageSource.getMessage("common.error")); 
- 			jsonResult.setErrMessage(e.getMessage());
-		} 		
- 		modelMap.put("jsonResult", jsonResult);		
-		return modelMap;
-    }
-	
-    @RequestMapping(value="/getAdmBannerId")
-	public ModelMap getAdmBannerId(ModelMap modelMap) throws Exception {
-    	String bnrId = "";
-    	JSONResult jsonResult = new JSONResult();
- 		try{
- 			bnrId = admBannerService.getAdmBannerId();
-		}catch(Exception e){
-			jsonResult.setSuccess(false);
-			jsonResult.setMessage(messageSource.getMessage("common.error")); 
- 			jsonResult.setErrMessage(e.getMessage());
-		}
-		modelMap.put("jsonResult", jsonResult);	
- 		modelMap.put("maxBnrId", bnrId);
- 		return modelMap;
-    }
-    
 	/**
 	 * 홍보배너 상세조회 
 	 * @param AdmSysBannerVO
@@ -270,67 +109,53 @@ public class AdmBannerController
 
 		if(admBannerVO.getBnrId() != null){
 			AdmBannerVO admBanner = admBannerService.getAdmBanner(admBannerVO);
-			List<AdmBannerVO> appendImgVO = admBannerService.getAdmBannerAppendImg(admBannerVO);
-			admBanner.setPageType("V");
+			AdmBannerApndFileVO admBannerApndFileVO = new AdmBannerApndFileVO();
+			admBannerApndFileVO.setBnrId(admBannerVO.getBnrId());
+			List<AdmBannerVO> appendImgVO = admBannerService.getAdmBannerAppendImg(admBannerApndFileVO);
+			if(appendImgVO.size() > 0){
+				modelMap.put("appendImg", appendImgVO.get(0));
+			}
 			
 			modelMap.put("admBanner", admBanner);
-			modelMap.put("appendImg", appendImgVO);
-		}
-		return ".adm/adm/banner/admBannerView";
-	}
-
-	/**
-	 * 홍보배너 이미지조회
-	 * @param AdmSysBannerVO
-	 * @return adm/banner/admBannerImgView
-	 * @exception Exception
-	 */
-	@RequestMapping(value="getAdmBannerImgView")
-	public String getAdmBannerImgView(
-								@ModelAttribute("admBannerVO") AdmBannerVO admBannerVO,
-								ModelMap modelMap)
-								throws Exception {
-
-		if(admBannerVO.getBnrId() != null){
-			List<AdmBannerVO> appendImgVO = admBannerService.getAdmBannerAppendImg(admBannerVO);
-			modelMap.put("appendImg", appendImgVO);
-			
-			String WEB_DIR = PortalxpertConfigUtils.getString("webserver.image.path");
-			modelMap.put("WEB_DIR", WEB_DIR);
-		}
-		return ".self/adm/banner/admBannerImgView";
-	}
-
-
-	/**
-	 * 홍보배너 수정
-	 * @param AdmSysBannerVO
-	 * @return adm/banner/admBannerUpdate
-	 * @exception Exception
-	 */
-	@RequestMapping(value="getAdmBannerUpdate")
-	public String getAdmBannerUpdate(
-								@ModelAttribute("admBannerVO") AdmBannerVO admBannerVO,
-								ModelMap modelMap)
-								throws Exception {
-
-		if(admBannerVO.getBnrId() != null){
-			AdmBannerVO admBanner = admBannerService.getAdmBanner(admBannerVO);
-			List<AdmBannerVO> appendImg = admBannerService.getAdmBannerAppendImg(admBannerVO);
-			admBanner.setPageType("U");
-			modelMap.put("admBanner", admBanner);
-			modelMap.put("appendImg", appendImg);
-			
-			//파일 첨부 관련
-			modelMap.put("innoApEnc" ,PortalxpertConfigUtils.getString("person.upload.innoap.enc"));
-			modelMap.put("innoApkey1" ,PortalxpertConfigUtils.getString("person.upload.innoap.key1"));
-			modelMap.put("innoApkey2" ,PortalxpertConfigUtils.getString("person.upload.innoap.key2"));
 		}
 		
-		return ".adm/adm/banner/admBannerUpdate";
-	}
+		//배너위치
+		AdmGenCodeManageVO admGenCodeManageVO = new AdmGenCodeManageVO();
+		admGenCodeManageVO.setCd("P_ROW_POS");
+		List<AdmGenCodeManageVO> admGenCommonCodeSpecList = admGenCodeManageService.getAdmGenCommonCodeSpecList(admGenCodeManageVO);
+		modelMap.put("parRowPosList", JSONUtils.objectToJSON(admGenCommonCodeSpecList));
+		
+		admGenCodeManageVO.setCd("ROW_POS");
+		admGenCommonCodeSpecList = admGenCodeManageService.getAdmGenCommonCodeSpecList(admGenCodeManageVO);
+		modelMap.put("rowPosList", JSONUtils.objectToJSON(admGenCommonCodeSpecList));
+		
+		return ".self/adm/banner/admBannerManage";
+	}	
+	
+    /**
+     * 이미지 파일 업로드
+     * @param modelMap
+     * @return board/bbsFileUpload.jsp
+     * @throws Exception
+     */
+    @RequestMapping("/bbsFileUpload") 
+    @ResponseBody 
+    public void bbsFileUpload(HttpServletRequest request, HttpServletResponse response, ModelMap model, HttpSession session) throws Exception{
+ 	  
+		String SAVE_DIR = PortalxpertConfigUtils.getString("upload.temp.dir");
+		String WEB_DIR = PortalxpertConfigUtils.getString("upload.temp.web");
+		String CONTEXT_PATH = PortalxpertConfigUtils.getString("image.web.contextpath");
+		int maxFileSize = PortalxpertConfigUtils.getInt("upload.file.size");
+		   
+		JSONArray jsonArr = FileUploadUtil.upload(request, SAVE_DIR, WEB_DIR, CONTEXT_PATH, maxFileSize);
+			
+		HttpServletResponseWrapper wrapper = new HttpServletResponseWrapper(response);
+		response.getWriter().print(jsonArr.toString());
+		response.getWriter().flush();
+		response.getWriter().close();
+ 	}
 
-
+    
 	/**
 	 * 홍보배너 등록 
 	 * @param AdmLinkUserInfoVO
@@ -338,16 +163,14 @@ public class AdmBannerController
 	 * @exception Exception
 	 */
 	@RequestMapping(value = "/insertAdmBanner", method = RequestMethod.POST)
-	public ModelMap insertAdmBanner(
-								@ModelAttribute("admBannerVO") AdmBannerVO admBannerVO,
+	public ModelMap insertAdmBanner(@RequestParam(value="data" ,required = true) String data,
 								ModelMap modelMap,
 								HttpSession session ) throws Exception {
 		
 		JSONResult jsonResult = new JSONResult();
 
 		try{
-			admBannerService.insertAdmBanner(admBannerVO, session);
-//			admBannerService.insertAdmBannerAppendImg(admBannerVO, session);
+			admBannerService.insertAdmBanner(data, session);
 			jsonResult.setSuccess(true);
 			jsonResult.setMessage(messageSource.getMessage("commom.ok"));
 	 		
@@ -360,98 +183,4 @@ public class AdmBannerController
 		modelMap.put("jsonResult", jsonResult);
 		return modelMap;
 	}
-
-	
-	/**
-	 * 홍보배너 수정
-	 * @param AdmBannerVO, HttpServletRequest
-	 * @return ModelMap
-	 * @exception Exception
-	 */
-	@RequestMapping(value = "/updateAdmBanner", method = RequestMethod.POST)
-	public ModelMap updateAdmBanner(
-									@ModelAttribute  
-									AdmBannerVO admBannerVO,
-									ModelMap modelMap,
-									HttpSession session ) throws Exception {
-
-		JSONResult jsonResult = new JSONResult();
-
-		try{
-//			admBannerService.updateAdmBannerAppendImg(admBannerVO, session);
-			admBannerService.updateAdmBanner(admBannerVO, session);
-			jsonResult.setSuccess(true);
-			jsonResult.setMessage(messageSource.getMessage("update.ok"));		
-		 }catch (Exception e) {
-		 	jsonResult.setSuccess(false);
-		 	jsonResult.setMessage(messageSource.getMessage("common.error"));
-		 	jsonResult.setErrMessage(e.getMessage());
-		}
-		modelMap.put("jsonResult", jsonResult);
-		return modelMap;
-	}
-  
-
-	/**
-	 * 홍보배너 삭제(단건)
-	 * @param AdmBannerVO, HttpServletRequest
-	 * @return ModelMap
-	 * @exception Exception
-	 */
-	@RequestMapping(value = "/deleteAdmBanner", method = RequestMethod.POST)
-	public ModelMap deleteAdmBanner(
-									@ModelAttribute  
-										AdmBannerVO admBannerVO,
-										ModelMap modelMap,
-										HttpSession session ) throws Exception {
-				
-		JSONResult jsonResult = new JSONResult();
-				
-		try{
-
-			admBannerService.deleteAdmBannerAppendImg(admBannerVO, session);
-			admBannerService.deleteAdmBanner(admBannerVO, session);
-			jsonResult.setSuccess(true);
-			jsonResult.setMessage(messageSource.getMessage("delete.ok"));
-						
-		}catch (Exception e) {
-			 jsonResult.setSuccess(false);
-			 jsonResult.setMessage(messageSource.getMessage("common.error"));
-			 jsonResult.setErrMessage(e.getMessage());
-		}
-		modelMap.put("jsonResult", jsonResult);
-				
-		return modelMap;
-	}  
-	
-	/**
-	 * 홍보배너 삭제(다건)
-	 * @param AdmBannerVO, HttpServletRequest
-	 * @return ModelMap
-	 * @exception Exception
-	 */
-	@RequestMapping(value = "/deleteAdmBanners", method = RequestMethod.POST)
-	public ModelMap deleteAdmBanners(
-									@ModelAttribute  
-										AdmBannerVO admBannerVO,
-										ModelMap modelMap,
-										HttpSession session ) throws Exception {
-				
-		JSONResult jsonResult = new JSONResult();
-		
-		try{
-			admBannerService.deleteAdmBannerAppendImgs(admBannerVO, session);
-			admBannerService.deleteAdmBanners(admBannerVO, session);
-			jsonResult.setSuccess(true);
-			jsonResult.setMessage(messageSource.getMessage("delete.ok"));
-						
-		}catch (Exception e) {
-			 jsonResult.setSuccess(false);
-			 jsonResult.setMessage(messageSource.getMessage("common.error"));
-			 jsonResult.setErrMessage(e.getMessage());
-		}
-		modelMap.put("jsonResult", jsonResult);
-				
-		return modelMap;
-	} 
 }
